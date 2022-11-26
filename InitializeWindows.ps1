e7809f1db42562125c67d2320e826ffb2bc4dd7c
Adam Radocz <adam.radocz@gmail.com>
2022-11-26 13:30:30 +0100

    Use winget to install apps.

---
+++ InitializeWindows.ps1
@@ -0,0 +1,17 @@
+$profiles = Get-ChildItem .\Profiles\
+
+Write-Host
+$index = 0
+foreach($profile in $profiles)
+{
+    Write-Host $index")" $profile.Name
+    $index++
+}
+
+Write-Host
+$selectedIndex = Read-Host "Enter the index of the selected profile"
+Write-Host "Selected profile:" $profiles[$selectedIndex] -ForegroundColor Green
+$appJsonPath = Join-Path -Path $profiles[$selectedIndex] "apps.json"
+
+winget source update
+winget import -i $appJsonPath --accept-package-agreements --accept-source-agreements
