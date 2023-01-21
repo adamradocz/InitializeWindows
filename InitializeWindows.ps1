9dcbbf8f77ee64b3371066609c17d0c27670c9f6
Adam Radocz <16503773+adamradocz@users.noreply.github.com>
2023-01-21 17:41:35 +0100

    Update InitializeWindows.ps1

--- InitializeWindows.ps1
+++ InitializeWindows.ps1
@@ -13,6 +13,17 @@ foreach($profile in $profiles)
 Write-Host
 $selectedIndex = Read-Host "Enter the index of the selected profile"
 Write-Host "Selected profile:" $profiles[$selectedIndex] -ForegroundColor Green
+
+Get-AppxPackage Clipchamp.Clipchamp* | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingNews* | Remove-AppxPackage
+Get-AppxPackage Microsoft.GamingApp* | Remove-AppxPackage
+Get-AppxPackage Microsoft.GetHelp* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Getstarted* | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage Microsoft.YourPhone* | Remove-AppxPackage
+
 $appJsonPath = Join-Path -Path $profiles[$selectedIndex] "apps.json"
 
 winget source update
