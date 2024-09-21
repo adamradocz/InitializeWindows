cc7bdb361f9f1f1c4dd1033a98c1ea3685568b54
Adam Radocz <16503773+adamradocz@users.noreply.github.com>
2024-09-21 21:07:16 +0200

    Update InitializeWindows.ps1

--- InitializeWindows.ps1
+++ InitializeWindows.ps1
@@ -1,19 +1,5 @@
 #Requires -RunAsAdministrator
 
-$profiles = Get-ChildItem .\Profiles\
-
-Write-Host
-$index = 0
-foreach($profile in $profiles)
-{
-    Write-Host $index")" $profile.Name
-    $index++
-}
-
-Write-Host
-$selectedIndex = Read-Host "Enter the index of the selected profile"
-Write-Host "Selected profile:" $profiles[$selectedIndex] -ForegroundColor Green
-
 Get-AppxPackage Clipchamp.Clipchamp* | Remove-AppxPackage
 Get-AppxPackage Microsoft.BingNews* | Remove-AppxPackage
 Get-AppxPackage Microsoft.GamingApp* | Remove-AppxPackage
@@ -25,8 +11,8 @@ Get-AppxPackage Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
 Get-AppxPackage Microsoft.YourPhone* | Remove-AppxPackage
 Get-AppxPackage MicrosoftTeams* | Remove-AppxPackage
 
-$appJsonPath = Join-Path -Path $profiles[$selectedIndex] "apps.json"
-
 winget source update
 winget upgrade --all
 winget import -i $appJsonPath --accept-package-agreements --accept-source-agreements
+
+winget install Ghisler.TotalCommander Google.Chrome Microsoft.PowerShell UCBerkeley.BOINC VideoLAN.VLC WireGuard.WireGuard
