ca3c9d99a92374379e2bca85755c9cf3d66118e4
Adam Radocz <16503773+adamradocz@users.noreply.github.com>
2024-10-28 12:11:48 +0100

    Update InitializeWindows.ps1

--- InitializeWindows.ps1
+++ InitializeWindows.ps1
@@ -1,15 +1,36 @@
 #Requires -RunAsAdministrator
 
-Get-AppxPackage Clipchamp.Clipchamp* | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingNews* | Remove-AppxPackage
-Get-AppxPackage Microsoft.GamingApp* | Remove-AppxPackage
-Get-AppxPackage Microsoft.GetHelp* | Remove-AppxPackage
-Get-AppxPackage Microsoft.Getstarted* | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage Microsoft.YourPhone* | Remove-AppxPackage
-Get-AppxPackage MicrosoftTeams* | Remove-AppxPackage
+#Infos: https://github.com/SimonCropp/WinDebloat
+winget uninstall --name "Windows Camera" --exact
+winget uninstall --name "Clipchamp" --exact
+winget uninstall --name "Feedback Hub" --exact
+winget uninstall --name "Films & TV" --exact
+winget uninstall --name "Solitaire & Casual Games" --exact
+winget uninstall --name "Microsoft Solitaire Collection" --exact
+winget uninstall --name "Get Help" --exact
+winget uninstall --name "Windows Maps" --exact
+winget uninstall --name "Windows Media Player" --exact
+winget uninstall --name "Mixed Reality Portal" --exact
+winget uninstall --name "Movies & TV" --exact
+winget uninstall --name "Microsoft News" --exact
+winget uninstall --name "News" --exact
+winget uninstall --name "Microsoft Photos" --exact
+winget uninstall --name "Print 3D" --exact
+winget uninstall --name "Skype" --exact
+winget uninstall --name "Microsoft Sticky Notes" --exact
+winget uninstall --name "Teams Machine-Wide Installer" --exact
+winget uninstall --name "Microsoft Tips" --exact
+winget uninstall --name "MSN Weather" --exact
+winget uninstall --name "Xbox TCUI" --exact
+winget uninstall --name "Xbox Console Companion" --exact
+winget uninstall --name "Xbox Game Bar Plugin" --exact
+winget uninstall --name "Xbox Game Speech Window" --exact
+winget uninstall --name "Xbox Game Bar" --exact
+winget uninstall --name "Xbox Accessories" --exact
+winget uninstall --name "Xbox" --exact
+winget uninstall --name "Phone Link" --exact
+winget uninstall --name "Quick Assist" --exact
+winget uninstall --name "Microsoft Teams" --exact
 
 winget source update
 winget upgrade --all
