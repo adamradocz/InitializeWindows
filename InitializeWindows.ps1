2f126d7cd458eea29000e572c805c02f4cdbc53f
Adam Radocz <16503773+adamradocz@users.noreply.github.com>
2024-10-30 13:12:09 +0100

    Update InitializeWindows.ps1

--- InitializeWindows.ps1
+++ InitializeWindows.ps1
@@ -1,9 +1,11 @@
 #Requires -RunAsAdministrator
 
-#Info: https://github.com/SimonCropp/WinDebloat
+# Info: https://github.com/SimonCropp/WinDebloat
 winget uninstall --exact --name "Clipchamp"
 winget uninstall --exact --name "Feedback Hub"
 winget uninstall --exact --name "Films & TV"
+winget uninstall --exact --name "Game Bar"
+winget uninstall --exact --name "Game Speech Window"
 winget uninstall --exact --name "Get Help"
 winget uninstall --exact --name "MSN Weather"
 winget uninstall --exact --name "Microsoft News"
@@ -22,6 +24,7 @@ winget uninstall --exact --name "Quick Assist"
 winget uninstall --exact --name "Skype"
 winget uninstall --exact --name "Solitaire & Casual Games"
 winget uninstall --exact --name "Teams Machine-Wide Installer"
+winget uninstall --exact --name "Web Search from Microsoft Bing"
 winget uninstall --exact --name "Windows Camera"
 winget uninstall --exact --name "Windows Maps"
 winget uninstall --exact --name "Windows Media Player"
@@ -30,6 +33,7 @@ winget uninstall --exact --name "Xbox Console Companion"
 winget uninstall --exact --name "Xbox Game Bar Plugin"
 winget uninstall --exact --name "Xbox Game Bar"
 winget uninstall --exact --name "Xbox Game Speech Window"
+winget uninstall --exact --name "Xbox Identity Provider"
 winget uninstall --exact --name "Xbox TCUI"
 winget uninstall --exact --name "Xbox"
 
@@ -38,10 +42,14 @@ winget upgrade --all
 winget install --exact --id Ghisler.TotalCommander
 winget install --exact --id Google.Chrome
 winget install --exact --id Gyan.FFmpeg
-winget install --exact --id Logitech.OptionsPlus 
 winget install --exact --id Microsoft.PowerShell
 winget install --exact --location $Env:LOCALAPPDATA\Microsoft\WinGet\Packages\TorProject.TorBrowser --id TorProject.TorBrowser
 winget install --exact --id UCBerkeley.BOINC
 winget install --exact --id VideoLAN.VLC
 winget install --exact --id WireGuard.WireGuard
 winget install --exact --id XnSoft.XnViewMP
+
+# Hardware specific
+winget install --exact --id Intel.IntelDriverAndSupportAssistant
+winget install --exact --id Logitech.OptionsPlus 
+winget install --exact --id Nvidia.GeForceExperience
