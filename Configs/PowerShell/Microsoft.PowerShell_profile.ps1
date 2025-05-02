cbbeb295e358696a4ca74c56bf19794e8eb7514c
Adam Radocz <adam.radocz@gmail.com>
2025-05-02 20:16:08 +0200

    Update oh my posh

--- Configs/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -12,5 +12,5 @@ Set-PSReadLineOption -EditMode Windows
 # Oh My Posh
 # The following snippet should be the last line in the PowerShell profile script.
 # Reference: https://ohmyposh.dev/docs/installation/prompt
-#oh-my-posh init pwsh --config "https://raw.githubusercontent.com/adamradocz/InitializeWindows/refs/heads/main/Configs/PowerShell/my.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config "C:\Users\Adam Radocz\OneDrive\Documents\PowerShell\my.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/adamradocz/InitializeWindows/refs/heads/main/Configs/PowerShell/my.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "C:\Users\Adam Radocz\OneDrive\Documents\PowerShell\my.omp.json" | Invoke-Expression
