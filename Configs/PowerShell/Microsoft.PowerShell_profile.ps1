35028770b5d5a27044eb6a5905718af5732d519b
Adam Radocz <adam.radocz@gmail.com>
2025-05-04 20:45:54 +0200

    Update PowerShell

--- Configs/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,32 @@
-# Terminal-Icons
+#region Terminal-Icons
 # Reference: https://github.com/devblackops/Terminal-Icons
+#*********************************************************************
 Import-Module -Name Terminal-Icons
+#endregion
 
-# PSReadline
+#region PSReadline
 # Reference: https://www.hanselman.com/blog/adding-predictive-intellisense-to-my-windows-terminal-powershell-prompt-with-psreadline
+#*********************************************************************
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
-# Oh My Posh
+# This will create a custom function.
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+b `
+                         -BriefDescription BuildCurrentDirectory `
+                         -Description "Run: 'dotnet build'." `
+                         -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet build")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+#endregion
+
+#region Oh My Posh
 # The following snippet should be the last line in the PowerShell profile script.
 # Reference: https://ohmyposh.dev/docs/installation/prompt
-oh-my-posh init pwsh --config "https://raw.githubusercontent.com/adamradocz/InitializeWindows/refs/heads/main/Configs/PowerShell/my.omp.json" | Invoke-Expression
-#oh-my-posh init pwsh --config "C:\Users\Adam Radocz\OneDrive\Documents\PowerShell\my.omp.json" | Invoke-Expression
+#*********************************************************************
+#oh-my-posh init pwsh --config "https://raw.githubusercontent.com/adamradocz/InitializeWindows/refs/heads/main/Configs/PowerShell/my.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\Adam Radocz\OneDrive\Documents\PowerShell\my.omp.json" | Invoke-Expression
+#endregion
--- Initialize-Windows.ps1
+++ Initialize-Windows.ps1
@@ -26,12 +26,13 @@ winget upgrade --all --include-unknown --verbose
 winget install --exact --id Ghisler.TotalCommander
 winget install --exact --id Google.Chrome
 winget install --exact --id Gyan.FFmpeg
-winget install --exact --id JanDeDobbeleer.OhMyPosh
 
 winget install --exact --id Microsoft.Office
 winget pin add --id Microsoft.Office --verbose # Office upgrade does not work. Source: https://github.com/microsoft/winget-cli/issues/1728#issuecomment-2467854561
 
+# PowerShell and everything related to it
 winget install --exact --id Microsoft.PowerShell
+winget install --exact --id JanDeDobbeleer.OhMyPosh
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module -Name PSReadLine -Repository PSGallery
 
